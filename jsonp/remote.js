7063ef25ecf6f29d7d25a91c2e5af6a417fa980f
tgxhx <tgxh2020@gmail.com>
2017-02-18 18:32:37 +0800

    p

--- jsonp/remote.js
+++ jsonp/remote.js
@@ -1,3 +1,29 @@
 localHandler({
-    "result": "我是远程js带来的数据"
+    "Conference": "Future Marketing",
+    "Date": "2016-11-22",
+    "Address": "Beijing",
+    "Member": [
+        {
+            "name": "Bob",
+            "age": 32,
+            "Company": "IBM",
+            "Engineer": true
+        },
+        {
+            "name": "John",
+            "age": 20,
+            "Company": "Oracle",
+            "Engineer": true
+        },
+        {
+            "name": "Henry",
+            "age": 33,
+            "Company": "Google",
+            "Engineer": true,
+            "Tel": {
+                "id": 138,
+                "phone": "samsung"
+            }
+        }
+    ]
 });
